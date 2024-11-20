60e4ceef3b484db881040084c4370d7c0e79125b
michelineuh <gaelschenck@hotmail.fr>
2024-11-20 14:53:16 +0100

    v1 title+liste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Paper } from './models/paper';
+import { PaperTitleComponent } from "./components/paper-title/paper-title.component";
+import { PaperListComponent } from "./components/paper-list/paper-list.component";
+
+
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PaperTitleComponent, PaperListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'B32024-angular-tornabene';
+ 
+  public papers: Paper[] = [
+    {id:1 , name:"Papier 1 " ,texture:"Lisse" , grammage:"80gr" , color:"blanc" },
+    {id:2 , name:"Papier 2" ,texture:"Grain fin" , grammage:"120gr" , color:"écru" }
+  ];
+  public selectedPaper: Paper | undefined;
+
+  onSelectPaper($event: Paper) {
+    this.selectedPaper = $event;
+  }
+
+  public onSubmit(paper: Paper){
+    this.papers.push(paper);
+    this.selectedPaper = paper;
+  }
 }
-//coucou
+
