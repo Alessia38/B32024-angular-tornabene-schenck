d78cdca83dc34a0b9d5d30fc36d3159df22d512a
michelineuh <gaelschenck@hotmail.fr>
2024-11-21 17:36:54 +0100

    v3 form-validators +errors

--- src/app/components/paper-form/paper-form.component.ts
+++ src/app/components/paper-form/paper-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Paper } from '../../models/paper';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -20,14 +20,22 @@ export class PaperFormComponent implements OnChanges {
 
   constructor() {
     this.paperForm = new FormGroup({
-      name: new FormControl('', Validators.required),
-      texture: new FormControl(''),
-      grammage: new FormControl(''),
-      color: new FormControl(''),
+      name: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      texture: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+      grammage: new FormControl('', [ 
+        Validators.required, 
+        Validators.pattern(/^\d+gr$/)
+        // Regex pour valider "20gr", "250gr", etc. ne pas mettre d'espace
+      ]),
+      color: new FormControl('', Validators.required),
     });
   }
+  public onNew(){
+    // reset des valeurs du formulaire
+    this.paperForm.reset();
+  }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  public ngOnChanges(changes: SimpleChanges): void {
     if (changes['selectedPaper'] && this.selectedPaper != null) {
       this.paperForm.setValue({
         id : this.selectedPaper.id,
@@ -39,14 +47,31 @@ export class PaperFormComponent implements OnChanges {
     }
   }
 
-  onSubmit() {
+  public onSubmit() {
     if (this.paperForm.valid) {
       const updatedPaper: Paper = this.paperForm.value;
       this.savePaper.emit(updatedPaper);
     }
   }
 
-  onCancel() {
+  public onCancel() {
     this.cancelEdit.emit();
   }
+
+  public showFieldError(fieldName: string, errorKind: string = ''): boolean{
+    if (this.paperForm.get(fieldName)?.touched) {
+      if (errorKind == ''){
+        return !this.paperForm.get(fieldName)?.valid;
+      }else{
+        return this.paperForm.get(fieldName)?.errors?.[errorKind] != null;
+      }
+    }else{
+      return false;
+    }
+  }
+  
+  public getError(fieldName: string){
+    return this.paperForm.get(fieldName)?.errors;
+  }
 }
+
