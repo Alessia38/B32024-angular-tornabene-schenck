30ac88945df9f68c7e8f80f243e50e07233456ec
Alessia38 <alessia.tornabene16@gmail.com>
2025-01-17 11:30:27 +0100

    Initial commit for backend

---
+++ Backend/Paper.Api/Paper.Api/Models/Paper.cs
@@ -0,0 +1,11 @@
+﻿namespace Paper.Api.Models
+{
+    public class Paper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "" ;
+        public string Texture { get; set; } = "";
+        public string Grammage { get; set; } = "";
+        public string Color { get; set; } = ""; 
+    }
+}
